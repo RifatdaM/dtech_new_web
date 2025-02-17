4fa654d2009e95f8e9ae1d9ff2be5c647759edfe
Rakibul Hasan <39544817+RifatdaM@users.noreply.github.com>
2025-02-17 16:39:10 +0600

    font issue fixed

--- vite.config.ts
+++ vite.config.ts
@@ -16,27 +16,32 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '@fonts': path.resolve(__dirname, 'src/assets/fonts'),
     }
   },
   build: {
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          if (!assetInfo.name) return 'assets/[name]-[hash][extname]';
-          
-          const info = assetInfo.name.split('.');
-          const extType = info[info.length - 1];
-          
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            return `assets/img/[name]-[hash][extname]`;
-          }
-          if (/woff|woff2|ttf|otf|eot/i.test(extType)) {
-            return `assets/fonts/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          styles: ['bootstrap', 'sass']
         },
+        assetFileNames: ({ name }) => {
+          if (!name) return 'assets/[name]-[hash][extname]';
+          
+          if (name.match(/\.(woff2?|ttf|eot)$/)) {
+            return 'assets/fonts/[name]-[hash][extname]';
+          }
+          
+          if (name.match(/\.(png|jpe?g|gif|svg|webp)$/)) {
+            return 'assets/img/[name]-[hash][extname]';
+          }
+          
+          return 'assets/[name]-[hash][extname]';
+        }
       }
     }
-  }
+  },
+  publicDir: 'public'
 });
